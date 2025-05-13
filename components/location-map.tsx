dcc2929c072585e3a6a6113d01f77358afa8fdd2
bantoinese83 <b.antoine.se@gmail.com>
2025-05-12 22:49:44 -0400

    Fix ReferenceError: React is not defined (add import for React)

--- components/location-map.tsx
+++ components/location-map.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import type React from "react"
+import React from "react"
 
 import { useState, useEffect, useRef, useCallback } from "react"
 import { Loader } from "@googlemaps/js-api-loader"
